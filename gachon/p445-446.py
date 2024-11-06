92d99956fff9b29beafb94fd63d414c570d60549
xxerone <gkstjd6097@gmail.com>
2024-11-06 14:20:40 +0900

    feat: transfer_p445-446.py

---
+++ gachon/p445-446.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.colorchooser import askcolor
+
+DEFAULT_PEN_SIZE = 1.0
+DEFAULT_COLOR = "black"
+
+mode = "pen"
+old_x = None
+old_y = None
+mycolor = DEFAULT_COLOR
+erase_on = False
