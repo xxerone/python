0565941a33df51f7991b2df764b1e75cad3dba89
xxerone <gkstjd6097@gmail.com>
2024-11-06 14:21:59 +0900

    transfer_p544.py

---
+++ gachon/p544.py
@@ -0,0 +1,12 @@
+# class MyEnumerate:
+#     def __init__(self, index, letter):
+        
+#     def __iter__(self):
+#         return self
+    
+#     def __next__(self):
+        
+
+# M = MyEnumerate(0, )
+# for index, letter in MyEnumerate('abc'):
+#     print(f"{index} : {letter}")
